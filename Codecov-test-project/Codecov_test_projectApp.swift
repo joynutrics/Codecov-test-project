8190e140bfcd4ac13488cc40dfe9d070a0a1f8d7
Stefan Gerard (D066945) <stefan.gerard@sap.com>
2020-10-26 23:13:18 +0100

    Initial Commit

---
+++ Codecov-test-project/Codecov_test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codecov_test_projectApp.swift
+//  Codecov-test-project
+//
+//  Created by Gerard, Stefan on 26.10.20.
+//
+
+import SwiftUI
+
+@main
+struct Codecov_test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codecov-test-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Codecov-test-project
+//
+//  Created by Gerard, Stefan on 26.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
